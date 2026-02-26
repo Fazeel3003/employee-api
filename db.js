9b3245362679bf5d55ea0cce65fc92bee85a9f81
Fazeel <fmemon@xtsworld.in>
2026-02-26 10:54:51 +0530

    refactor: move database configuration to environment variables using dotenv

--- db.js
+++ db.js
@@ -1,18 +1,10 @@
 const mysql = require("mysql2");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "employee_management_system",
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
-connection.connect((err) => {
-  if (err) {
-    console.error("Database connection failed:", err);
-  } else {
-    console.log("Connected to MySQL database");
-  }
-});
-
-module.exports = connection;
+module.exports = db;
