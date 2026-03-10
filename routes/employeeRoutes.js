2f360074de7cd10778af9bf922914697af5291e4
Fazeel <fmemon@xtsworld.in>
2026-03-10 17:38:32 +0530

    feat: Add authentication middleware to employee routes

--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -1,12 +1,19 @@
 const validateEmployee = require("../middleware/validateEmployee");
+const { verifyToken, verifyRole } = require("../middleware/authMiddleware");
 const express = require("express");
 const router = express.Router();
 const employeeController = require("../controllers/employeeController");
 
-router.get("/", employeeController.getAllEmployees);
+// Apply authentication to all employee routes
+router.use(verifyToken);
+
+// Public routes (with authentication)
+router.get("/", verifyRole(['admin', 'manager']), employeeController.getAllEmployees);
 router.get("/:id", employeeController.getEmployeeById);
-router.post("/", validateEmployee, employeeController.createEmployee);
-router.put("/:id", validateEmployee, employeeController.updateEmployee);
-router.delete("/:id", employeeController.deleteEmployee);
+
+// Protected routes (with role-based access)
+router.post("/", verifyRole(['admin']), validateEmployee, employeeController.createEmployee);
+router.put("/:id", verifyRole(['admin']), validateEmployee, employeeController.updateEmployee);
+router.delete("/:id", verifyRole(['admin']), employeeController.deleteEmployee);
 
 module.exports = router;
